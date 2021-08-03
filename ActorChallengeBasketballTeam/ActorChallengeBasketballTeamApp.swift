af9189a18d88b9b88e41cd2cbe853bdf59642323
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 13:45:23 -0400

    Initial Commit

---
+++ ActorChallengeBasketballTeam/ActorChallengeBasketballTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorChallengeBasketballTeamApp.swift
+//  ActorChallengeBasketballTeam
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct ActorChallengeBasketballTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
